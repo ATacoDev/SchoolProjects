cb74e06f8eb94c0bbf13f94f0fd8ecadda94a90c
ATacoDev <Taco072103@gmail.com>
2023-09-26 02:31:57 -0700

    test commit

--- StartButton.cs
+++ StartButton.cs
@@ -19,7 +19,7 @@ public class StartButton : MonoBehaviour
         startButton.SetActive(false);
     }
     
-    public void showButton() // show buttons
+    public void showButton() // show button
     {
         startButton.SetActive(true);
     }
