58a246aacbb51420ed10df22ca3e7180b34c7a5c
ATacoDev <Taco072103@gmail.com>
2023-09-26 02:02:16 -0700

    test

---
+++ GameHandler.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour
+{
+    public StartButton startMenuButton;
+    public StartButton startGameButton;
+
+    public GameObject startScreen;
+    public GameObject gameScreen;
+    
+    void Start()
+    {
+        startMenuButton.showButton();
+        startGameButton.hideButton();
+        startScreen.SetActive(true);
+        gameScreen.SetActive(false);
+    }
+
+    public void startGame()
+    {
+        startScreen.SetActive(false);
+        gameScreen.SetActive(true);
+        startGameButton.showButton();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
