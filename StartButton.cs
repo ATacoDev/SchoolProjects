70e7f9e3d5afd57c794bcdc4d08c129e4cad8d54
ATacoDev <Taco072103@gmail.com>
2023-09-26 02:05:07 -0700

    Start button code

---
+++ StartButton.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartButton : MonoBehaviour
+{
+    public GameObject startButton;
+    public GameHandler gameHandler;
+
+    public void onClick()
+    {
+        hideButton();
+        gameHandler.startGame();
+    }
+
+    public void hideButton()
+    {
+        startButton.SetActive(false);
+    }
+    
+    public void showButton()
+    {
+        startButton.SetActive(true);
+    }
+}
