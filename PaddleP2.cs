f05ccf69b5a5e732e0a5d8deef47805e9980d3ac
ATacoDev <Taco072103@gmail.com>
2023-09-27 12:33:25 -0700

    Script for second player, might make this player a bot

---
+++ PaddleP2.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PaddleP2 : Paddle
+{
+
+    public GameObject paddle;
+    private Vector2 _direction;
+
+    private void FixedUpdate()
+    {
+        if (_direction.sqrMagnitude != 0)
+        {
+            _rigidbody.AddForce(_direction * speed);
+        }
+    }
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            _direction = Vector2.down;
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            _direction = Vector2.up;
+        }
+        else
+        {
+            _direction = Vector2.zero;
+        }
+    }
+
+}
