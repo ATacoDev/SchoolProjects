d39d552d66c63f7752fe3fade69106c544272fbe
ATacoDev <Taco072103@gmail.com>
2023-09-27 12:29:30 -0700

    Fixed border jittering

--- PaddleP1.cs
+++ PaddleP1.cs
@@ -21,12 +21,10 @@ public class PaddleP1 : Paddle
    {
       if (Input.GetKey(KeyCode.S))
       {
-         transform.Translate(Vector2.down * Time.deltaTime * 10f);
          _direction = Vector2.down;
       }
       else if (Input.GetKey(KeyCode.W))
       {
-         transform.Translate(Vector2.up * Time.deltaTime * 10f);
          _direction = Vector2.up;
       }
       else
