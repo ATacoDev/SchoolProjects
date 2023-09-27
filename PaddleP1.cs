22a5f39193d3074a2a4f81ace7de6dea240a3e4c
ATacoDev <Taco072103@gmail.com>
2023-09-27 12:11:40 -0700

    Changing class to inherit from Paddle script

--- PaddleP1.cs
+++ PaddleP1.cs
@@ -1,29 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PaddleP1 : MonoBehaviour
+public class PaddleP1 : Paddle
 {
 
    public GameObject paddle;
+   private Vector2 _direction;
 
-   public void Update()
+   private void FixedUpdate()
    {
-      Move();
-      // if paddle hits a border, print ("Paddle hit a border") to console
+      if (_direction.sqrMagnitude != 0)
+      {
+         _rigidbody.AddForce(_direction * speed);
+      }
    }
-
-   private void Move() 
+   private void Update()
    {
       if (Input.GetKey(KeyCode.S))
       {
-         transform.Translate(Vector3.down * Time.deltaTime * 10f);
+         transform.Translate(Vector2.down * Time.deltaTime * 10f);
+         _direction = Vector2.down;
       }
       else if (Input.GetKey(KeyCode.W))
       {
-         transform.Translate(Vector3.up * Time.deltaTime * 10f);
+         transform.Translate(Vector2.up * Time.deltaTime * 10f);
+         _direction = Vector2.up;
+      }
+      else
+      {
+         _direction = Vector2.zero;
       }
    }
-   
+
 }
